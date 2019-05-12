0edbbea0b677c12a6540895b74563b997b1ae019
EC2 Default User <ec2-user@ip-172-31-42-33.eu-west-3.compute.internal>
2019-05-12 15:50:52 +0000

    Redirection of LoadBalancer

--- load_balancer/LoadBalancer.java
+++ load_balancer/LoadBalancer.java
@@ -1,5 +1,7 @@
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,10 +15,14 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.sun.jndi.toolkit.url.UrlUtil;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import pt.ulisboa.tecnico.cnv.solver.Solver;
 import pt.ulisboa.tecnico.cnv.solver.SolverArgumentParser;
 import pt.ulisboa.tecnico.cnv.solver.SolverFactory;
@@ -49,9 +55,9 @@ public class LoadBalancer{
 
                 final HttpServer load_balancer = HttpServer.create(new InetSocketAddress(8000),0);
 		
-		load_balancer.createContext("/climb", new MyHandler());
+                load_balancer.createContext("/climb", new SendQueryHandler());
 
-                //server.createContext("/test", new MyTestHandler());
+                load_balancer.createContext("/test", new MyTestHandler());
 
                 // be aware! infinite pool of threads!
                 load_balancer.setExecutor(Executors.newCachedThreadPool());
@@ -60,31 +66,72 @@ public class LoadBalancer{
                 System.out.println(load_balancer.getAddress().toString());
 		
 	}
-	
-
-	static class MyHandler implements HttpHandler {
+	        
+        static class MyTestHandler implements HttpHandler {
                 @Override
-                public void handle(final HttpExchange t) throws IOException {
-                        // Get the query.
-			final String query = t.getRequestURI().getQuery();
-
-			System.out.println("> Query:\t" + query);
-
-			System.out.println("> Thread id:\t" + Thread.currentThread().getId());
-
-			// Break it down into String[].
-                        final String[] params = query.split("&");
+		public void handle(final HttpExchange t) throws IOException {
+                        final Headers headers = t.getResponseHeaders();
                         
-                        //Get all the parameters
-                        System.out.println(params);
+                        final String query = t.getRequestURI().getQuery();
+
+                        System.out.println("> Query:\t" + query);
                         
-                        //Try to estimate the wheight of the query, by its params
+			String response = "test ok";
+				
+			t.sendResponseHeaders(200, response.getBytes().length);
 
-                        //Get a way of knowing the instances online
-			
+			headers.add("Access-Control-Allow-Origin", "*");
+			headers.add("Access-Control-Allow-Credentials", "true");
+			headers.add("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
+			headers.add("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
-                }
+	
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
 
 	}
+	static class SendQueryHandler implements HttpHandler{
+		@Override
+		public void handle(final HttpExchange t) throws IOException{
+                	final Headers headers = t.getResponseHeaders();
+                       
+			System.out.println("----------NEW REQUEST----------\t");
+	 
+                	final String query = t.getRequestURI().getQuery();
+			
+                	System.out.println("> Headers:\t" + query);
+                	System.out.println("> Query:\t" + query);
+			
+			//DNS name
+                	String DNSName="ec2-35-180-190-107.eu-west-3.compute.amazonaws.com:8000";		
+                	URL url = new URL("http://"+DNSName+t.getRequestURI().toString());
+                	HttpURLConnection con = (HttpURLConnection) url.openConnection();
+               
+                	// Send request
+                	con.setRequestMethod("GET");
+
+			//Get respons
+                      
+                	/*int responseCode = con.getResponseCode();
+			
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+    			String inputLine;
+    			StringBuffer response = new StringBuffer();
+
+    			while ((inputLine = in.readLine()) != null) {
+        			response.append(inputLine);
+    			}
+    			in.close();
+
+			String result=response.toString();
+    			System.out.println(result);*/
+
+			System.out.println("-------------------------------\t");
+                }
+	} 
+
 }
 	
